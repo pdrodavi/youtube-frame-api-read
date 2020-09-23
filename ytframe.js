e90dc42aabdb0b402837f4256392701d6d56de9f
Pedro Davi <31486060+pdrodavi@users.noreply.github.com>
2020-09-22 22:42:33 -0300

    Update ytframe.js

--- ytframe.js
+++ ytframe.js
@@ -1,15 +1,24 @@
 function onYouTubeIframeAPIReady() {
-    var e = document.getElementById("youtube-audio"), t = document.createElement("img");
-    t.setAttribute("id", "youtube-icon"), t.style.cssText = "cursor:pointer;cursor:hand", e.appendChild(t);
+    
+    var e = document.getElementById("youtube-audio"), 
+    var t = document.createElement("img");
+    
+    t.setAttribute("id", "youtube-icon"), 
+    t.style.cssText = "cursor:pointer;cursor:hand", e.appendChild(t);
+    
     var a = document.createElement("div");
-    a.setAttribute("id", "youtube-player"), e.appendChild(a);
+    a.setAttribute("id", "youtube-player"), 
+    e.appendChild(a);
+    
     var o = function (e) {
         var a = e ? "EQlGjkW.png" : "FSDW3V6.png";
         t.setAttribute("src", "https://i.imgur.com/" + a)
     };
+    
     e.onclick = function () {
         r.getPlayerState() === YT.PlayerState.PLAYING || r.getPlayerState() === YT.PlayerState.BUFFERING ? (r.pauseVideo(), o(!1)) : (r.playVideo(), o(!0))
     };
+    
     var r = new YT.Player("youtube-player", {
         height: "0",
         width: "0",
